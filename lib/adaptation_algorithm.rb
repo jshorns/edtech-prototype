806571750cd93e40aff6133a29fc23b40927f5d3
Joey <joey.s.hornsby@gmail.com>
2021-05-08 16:24:43 +0100

    added algorithm for averages based on past 3 questions

--- lib/adaptation_algorithm.rb
+++ lib/adaptation_algorithm.rb
@@ -8,4 +8,15 @@ class AdaptationAlgorithm
     end
   end
 
+  def last_three_qs(student_record)
+    case student_record.three_question_average
+    when  0..35
+      student_record.last_question.difficulty - 1
+    when 35..65
+      student_record.last_question.difficulty 
+    when 65..100
+      student_record.last_question.difficulty + 1
+    end
+  end
+  
 end
--- spec/adaptation_algorithm_spec.rb
+++ spec/adaptation_algorithm_spec.rb
@@ -1,11 +1,13 @@
 require 'adaptation_algorithm'
 
 describe AdaptationAlgorithm do
+
+  let(:mid_level_correct_answer) { double(:question, difficulty: 2, :correctly_answered? => true) }
+  let(:mid_level_incorrect_answer) { double(:question, difficulty: 2, :correctly_answered? => false) }
+
   describe '#default' do
-    let(:mid_level_correct_answer) { double(:question, difficulty: 2, :correctly_answered? => true) }
     let(:good_student_record) { double(:student_record, 
                                 :last_question=> mid_level_correct_answer) }
-    let(:mid_level_incorrect_answer) { double(:question, difficulty: 2, :correctly_answered? => false) }
     let(:bad_student_record) { double(:student_record, 
                                 :last_question=> mid_level_incorrect_answer) }
 
@@ -16,4 +18,24 @@ describe AdaptationAlgorithm do
       expect(subject.default(bad_student_record)).to eq 1
     end
   end
+
+  #questions over speed of a response here. if a student gets 3 qs wrong in a row, do we want them to fall down two levels? and likewise for going up levels?
+  #possibility of percentiles based on difficulty range. assuming that there are no difficulty levels that are blank.
+  describe '#last_three_qs' do
+  
+    let(:poor_student_record) { double(:student_record, :three_question_average => 30.00, :last_question => mid_level_incorrect_answer) }
+    let(:average_student_record) { double(:student_record, :three_question_average => 60.00, :last_question => mid_level_incorrect_answer) }
+    let(:good_student_record) { double(:student_record, :three_question_average => 90.00, :last_question => mid_level_correct_answer) }
+    
+    it 'should go down a level if 3 question average is in bottom third' do
+      expect(subject.last_three_qs(poor_student_record)).to eq poor_student_record.last_question.difficulty - 1
+    end
+    it 'should stay the same if 3 question average is in bottom third' do
+      expect(subject.last_three_qs(average_student_record)).to eq poor_student_record.last_question.difficulty
+    end
+    it 'should go up a level if 3 question average is in top third' do
+      expect(subject.last_three_qs(good_student_record)).to eq poor_student_record.last_question.difficulty + 1
+    end
+
+  end 
 end
