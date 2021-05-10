20ac1dd09f81d4d41a804879627dd317ad561ce5
Joey <joey.s.hornsby@gmail.com>
2021-05-10 19:23:20 +0100

    added averaging methods to student record

--- lib/student_record.rb
+++ lib/student_record.rb
@@ -10,12 +10,21 @@ class StudentRecord
   end
 
   def last_question_result
+    if @record.last.correctly_answered?
+      100
+    else
+      0
+    end
   end
 
   def three_question_average
+    correct = @record.last(3).select { |q| q.correctly_answered? }.size.to_f
+    (correct/3 * 100).round(2)
   end
 
   def all_time_average
+    correct = @record.select { |q| q.correctly_answered? }.size.to_f
+    (correct/@record.length * 100).round(2)
   end
 
 end
--- spec/student_record_spec.rb
+++ spec/student_record_spec.rb
@@ -1,14 +1,11 @@
 require 'student_record'
 
 describe StudentRecord do
-  let(:question1) { double(:question, question_text: "Is pudding a cat or a dog?", 
-  answer_a: "cat", 
-  answer_b: "dog", 
-  answer_c: "monkey", 
-  answer_d: "fish", 
-difficulty: 1, 
-correct_answer: :a
-)}
+  
+let(:correct_answer) { double(:question, :correctly_answered? => true) }
+let(:incorrect_answer) { double(:question, :correctly_answered? => false) }
+
+
 
   it { is_expected.to respond_to(:record) }
   it { is_expected.to respond_to(:last_question_result) }
@@ -17,10 +14,50 @@ correct_answer: :a
 
   describe '#add_question' do
     it 'pushes a question to the @record array' do
-      subject.add_question(question1)
-      expect(subject.record).to include(question1)
+      subject.add_question(correct_answer)
+      expect(subject.record).to include(correct_answer)
     end
   end
+
+  describe '#last question result' do
+    it 'should return 100 if last question correctly answered' do
+      subject.add_question(correct_answer)
+      expect(subject.last_question_result).to eq 100
+    end
+    it 'should return 0 if last question incorrectly answered' do
+      subject.add_question(incorrect_answer)
+      expect(subject.last_question_result).to eq 0
+    end
+
+  end
+
+  describe '#all_time_average' do
+    it 'should return 90 if 10 questions, 9 correctly answered' do
+      9.times { subject.add_question(correct_answer) }
+      subject.add_question(incorrect_answer)
+      expect(subject.all_time_average).to eq 90
+    end
+    it 'should return 10 if 10 questions, 1 correctly answered' do
+      9.times { subject.add_question(incorrect_answer) }
+      subject.add_question(correct_answer)
+      expect(subject.all_time_average).to eq 10
+    end
+
+  end
+
+  describe '#three_question_average' do
+  it 'should return 66.67 if 10 questions, of last 3 questions, 2 correctly answered' do
+    9.times { subject.add_question(correct_answer) }
+    subject.add_question(incorrect_answer)
+    expect(subject.three_question_average).to eq 66.67
+  end
+  it 'should return 33.33 if 10 questions, of last 3, 1 correctly answered' do
+    9.times { subject.add_question(incorrect_answer) }
+    subject.add_question(correct_answer)
+    expect(subject.three_question_average).to eq 33.33
+  end
+
+end
 
   
 
